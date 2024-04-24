70180b5c41c45948fccd5ba7b2212de70476f7f3
Mark Ingram <34866382+markingram84@users.noreply.github.com>
2024-04-24 15:29:26 +0100

    2.4.2 again

--- wdio.conf.js
+++ wdio.conf.js
@@ -49,20 +49,12 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-      /*capabilities: [
-    {
-      browserName: "chrome",
-    },
-  ],*/
-
-  capabilities: [
-    {
-      browserName: "chrome",
-      "goog:chromeOptions": {
-        args: ["headless", "disable-gpu"],
-      },
-    },
-  ],
+    capabilities: [{
+      browserName: 'chrome', 
+      'goog:chromeOptions': {
+          args: ['headless', 'disable-gpu']
+      }
+  }],
 
     //
     // ===================
