957ab0e619ab323916ac251707eb407f37ced8bb
Mark Ingram <34866382+markingram84@users.noreply.github.com>
2024-05-09 09:19:22 +0100

    take 15282

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,7 @@ exports.config = {
     capabilities: [{
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu']
+            args: ['headless', '--disable-gpu']
         }
     }],
 
