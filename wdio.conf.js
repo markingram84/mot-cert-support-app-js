f39e29e992d4e05fc256215a5cfafa06d018f269
Mark Ingram <34866382+markingram84@users.noreply.github.com>
2024-05-24 13:27:19 +0100

    missed a character

--- wdio.conf.js
+++ wdio.conf.js
@@ -131,7 +131,7 @@ exports.config = {
         outputFileFormat: function(options) {
             return 'junit-report.xml'
         }
-    }]]
+    }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
