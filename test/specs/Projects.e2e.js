c6502ea7a6353d569fb3627997c07fb53bf9d207
Mark Ingram <34866382+markingram84@users.noreply.github.com>
2024-06-13 13:56:52 +0100

    Update Projects.e2e.js

--- test/specs/Projects.e2e.js
+++ test/specs/Projects.e2e.js
@@ -1,7 +1,9 @@
 const { expect, browser, $ } = require('@wdio/globals')
 
 describe('My Login application', () => {
-
+    beforeEach(() => {
+        browser.execute('window.localStorage.clear()')
+    })
     it('should show projects after creation', async () => {
         await browser.url('http://localhost:3000/#/login')
 
@@ -14,13 +16,13 @@ describe('My Login application', () => {
         await browser.url('http://localhost:3000/#/manage/projects')
 
         await $('#name').waitForExist()
-        await $('#title').setValue('Project 2')
+        await $('#name').setValue('Project 2')
         await $('.btn-primary').click()
 
         await browser.url('http://localhost:3000/#/projects')
 
-        const elements = await $$('.col-8 .list-group-item')
-        expect(elements.length).toBe(2)
+        const projectTableRows = await $('ul[data-testid="project-table"] tr')
+        expect(projectTableRows).toBeElementsArrayOfSize(2);
     });
 
     it('should show projects after login', async () => {
